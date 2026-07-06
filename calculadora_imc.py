f3ef4238e4d6577788509318bd2eb40263365b67
Giovani Luiz dos Reis <giovaniluizreis@gmail.com>
2026-07-06 16:01:41 -0300

    feat : calculadora_imc

---
+++ calculadora_imc.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+with st.sidebar:
+    st.title("Calculadora de IMC")
+    st.write("Esta calculadora permite calcular o Índice de Massa Corporal (IMC) com base no peso e altura fornecidos pelo usuário.")
+
